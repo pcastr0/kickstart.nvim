25728b96477f07e8a92c661211d9f37b206c094e
Patrick Castro <patrickp.castro@gmail.com>
2025-07-01 19:54:18 +0800

    add avante.nvim plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -24,96 +24,104 @@ return {
     ---@type render.md.UserConfig
     opts = {},
   },
+  -- {
+  --   'CopilotC-Nvim/CopilotChat.nvim',
+  --   dependencies = {
+  --     { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+  --     { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+  --   },
+  --   build = 'make tiktoken', -- Only on MacOS or Linux
+  --   opts = {
+  --     chat_autocomplete = false,
+  --     -- model = 'claude-3.7-sonnet',
+  --   },
+  --   keys = {
+  --     { '<leader>zc', ':CopilotChatToggle<CR>', mode = 'n', desc = 'Copilot Chat' },
+  --     { '<leader>ze', ':CopilotChatExplain<CR>', mode = 'v', desc = 'Explain Code' },
+  --     { '<leader>zr', ':CopilotChatReview<CR>', mode = 'v', desc = 'Review Code' },
+  --     { '<leader>zf', ':CopilotChatFix<CR>', mode = 'v', desc = 'Fix Code Issues' },
+  --     { '<leader>zo', ':CopilotChatOptimize<CR>', mode = 'v', desc = 'Optimize Code' },
+  --     { '<leader>zm', ':CopilotChatModels<CR>', mode = 'n', desc = 'Copilot Models' },
+  --   },
+  -- },
   {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    dependencies = {
-      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
-      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
-    },
-    build = 'make tiktoken', -- Only on MacOS or Linux
+    'yetone/avante.nvim',
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = function()
+      -- conditionally use the correct build system for the current OS
+      if vim.fn.has 'win32' == 1 then
+        return 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false'
+      else
+        return 'make'
+      end
+    end,
+    event = 'VeryLazy',
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
     opts = {
-      chat_autocomplete = false,
-      model = 'claude-3.7-sonnet',
+      -- add any opts here
+      -- for example
+      provider = 'copilot',
+      providers = {
+        -- claude = {
+        --   endpoint = 'https://api.anthropic.com',
+        --   model = 'claude-sonnet-4-20250514',
+        --   timeout = 30000, -- Timeout in milliseconds
+        --   extra_request_body = {
+        --     temperature = 0.75,
+        --     max_tokens = 20480,
+        --   },
+        -- },
+        copilot = {
+          endpoint = 'https://api.githubcopilot.com',
+          model = 'claude-sonnet-3.5',
+          timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 20480,
+          },
+        },
+      },
     },
-    keys = {
-      { '<leader>zc', ':CopilotChatToggle<CR>', mode = 'n', desc = 'Copilot Chat' },
-      { '<leader>ze', ':CopilotChatExplain<CR>', mode = 'v', desc = 'Explain Code' },
-      { '<leader>zr', ':CopilotChatReview<CR>', mode = 'v', desc = 'Review Code' },
-      { '<leader>zf', ':CopilotChatFix<CR>', mode = 'v', desc = 'Fix Code Issues' },
-      { '<leader>zo', ':CopilotChatOptimize<CR>', mode = 'v', desc = 'Optimize Code' },
-      { '<leader>zm', ':CopilotChatModels<CR>', mode = 'n', desc = 'Copilot Models' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'echasnovski/mini.pick', -- for file_selector provider mini.pick
+      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'ibhagwan/fzf-lua', -- for file_selector provider fzf
+      'stevearc/dressing.nvim', -- for input provider dressing
+      'folke/snacks.nvim', -- for input provider snacks
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
     },
   },
-  -- {
-  -- 'yetone/avante.nvim',
-  -- -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  -- -- ⚠️ must add this setting! ! !
-  -- build = function()
-  --   -- conditionally use the correct build system for the current OS
-  --   if vim.fn.has 'win32' == 1 then
-  --     return 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false'
-  --   else
-  --     return 'make'
-  --   end
-  -- end,
-  -- event = 'VeryLazy',
-  -- version = false, -- Never set this value to "*"! Never!
-  -- ---@module 'avante'
-  -- ---@type avante.Config
-  -- opts = {
-  --   -- add any opts here
-  --   -- for example
-  --   provider = 'copilot',
-  --   providers = {
-  --     claude = {
-  --       endpoint = 'https://api.anthropic.com',
-  --       model = 'claude-sonnet-4-20250514',
-  --       timeout = 30000, -- Timeout in milliseconds
-  --       extra_request_body = {
-  --         temperature = 0.75,
-  --         max_tokens = 20480,
-  --       },
-  --     },
-  --   },
-  -- },
-  -- dependencies = {
-  --   'nvim-lua/plenary.nvim',
-  --   'MunifTanjim/nui.nvim',
-  --   --- The below dependencies are optional,
-  --   'echasnovski/mini.pick', -- for file_selector provider mini.pick
-  --   'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-  --   'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
-  --   'ibhagwan/fzf-lua', -- for file_selector provider fzf
-  --   'stevearc/dressing.nvim', -- for input provider dressing
-  --   'folke/snacks.nvim', -- for input provider snacks
-  --   'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-  --   'zbirenbaum/copilot.lua', -- for providers='copilot'
-  --   {
-  --     -- support for image pasting
-  --     'HakonHarnes/img-clip.nvim',
-  --     event = 'VeryLazy',
-  --     opts = {
-  --       -- recommended settings
-  --       default = {
-  --         embed_image_as_base64 = false,
-  --         prompt_for_file_name = false,
-  --         drag_and_drop = {
-  --           insert_mode = true,
-  --         },
-  --         -- required for Windows users
-  --         use_absolute_path = true,
-  --       },
-  --     },
-  --   },
-  --   {
-  --     -- Make sure to set this up properly if you have lazy=true
-  --     'MeanderingProgrammer/render-markdown.nvim',
-  --     opts = {
-  --       file_types = { 'markdown', 'Avante' },
-  --     },
-  --     ft = { 'markdown', 'Avante' },
-  --   },
-  -- },
-  --
-  -- },
 }
